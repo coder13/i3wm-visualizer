29cfbdcfe20c69aa9f77e6944f074741197bdb34
Caleb <choover11@gmail.com>
2021-04-28 21:07:11 -0700

    added basic types for i3tree

---
+++ app/@types/declarations.d.ts
@@ -0,0 +1,56 @@
+type Split = 'splith' | 'splitv';
+
+type Rect = {
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+};
+
+type Gaps = {
+  inner: number,
+  outer: number,
+  top: number,
+  right: number,
+  bottom: number,
+  left: number,
+}
+
+interface I3Node {
+  id: number;
+  type: string;
+  orientation: 'none' | 'horizontal' | 'vertical';
+  scratchpad_state: 'none';
+  percent: number | null;
+  urgent: boolean;
+  marks: Array<any>;
+  focused: boolean;
+  layout: Split;
+  workspace_layout: string;
+  last_split_layout: Split;
+  border: string;
+  current_border_width: number;
+  rect: Rect;
+  deco_rect: Rect;
+  window_rect: Rect;
+  geometry: Rect;
+  name: string;
+  window: number | null;
+  window_type: string | null;
+  nodes: Array<I3Node>;
+  floating_nodes: Array<I3Node>;
+  focus: Array<number>;
+  fullscreen_mode: number;
+  sticky: boolean;
+  floating: string;
+  swallows: Array<any>;
+};
+
+interface I3Output extends I3Node {
+  output: string;
+};
+
+interface I3Workspace extends I3Output {
+  num: number,
+  gaps: Gaps
+};
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 const serverURI = 'http://localhost:8080'
 
 function App() {
-  const [tree, setTree] = useState<any>();
+  const [tree, setTree] = useState<I3Node | undefined>(undefined);
   const [error, setError] = useState<Error>();
 
   const fetchTree = () =>
@@ -20,11 +20,62 @@ function App() {
     fetchTree();
   }, []);
 
+  tree && console.log(Object.keys(tree));
+
   return (
     <div className="App">
-      {JSON.stringify(tree)}
+      {error && error.message && <p className="error">{error.name}<br/>{error.message}<br/>{error.stack}</p>}
+      {tree && <Tree tree={tree} /> }
     </div>
   );
+}
+
+interface TreeProps {
+  tree: I3Node | I3Output;
+};
+
+const Tree: React.FC<TreeProps> = ({ tree }) => {
+  return (
+    <ul>
+      <hr />
+      <li key={'id'}>id: {JSON.stringify(tree['id'])}</li>
+      <li key={'type'}>type: {JSON.stringify(tree['type'])}</li>
+      <li key={'orientation'}>orientation: {JSON.stringify(tree['orientation'])}</li>
+      <li key={'scratchpad_state'}>scratchpad_state: {JSON.stringify(tree['scratchpad_state'])}</li>
+      <li key={'percent'}>percent: {JSON.stringify(tree['percent'])}</li>
+      <li key={'urgent'}>urgent: {JSON.stringify(tree['urgent'])}</li>
+      <li key={'marks'}>marks: {JSON.stringify(tree['marks'])}</li>
+      <li key={'focused'}>focused: {JSON.stringify(tree['focused'])}</li>
+      <li key={'layout'}>layout: {JSON.stringify(tree['layout'])}</li>
+      <li key={'workspace_layout'}>workspace_layout: {JSON.stringify(tree['workspace_layout'])}</li>
+      <li key={'last_split_layout'}>last_split_layout: {JSON.stringify(tree['last_split_layout'])}</li>
+      <li key={'border'}>border: {JSON.stringify(tree['border'])}</li>
+      <li key={'current_border_width'}>current_border_width: {JSON.stringify(tree['current_border_width'])}</li>
+      <li key={'rect'}>rect: {JSON.stringify(tree['rect'])}</li>
+      <li key={'deco_rect'}>deco_rect: {JSON.stringify(tree['deco_rect'])}</li>
+      <li key={'window_rect'}>window_rect: {JSON.stringify(tree['window_rect'])}</li>
+      <li key={'geometry'}>geometry: {JSON.stringify(tree['geometry'])}</li>
+      <li key={'name'}>name: {JSON.stringify(tree['name'])}</li>
+      <li key={'window'}>window: {JSON.stringify(tree['window'])}</li>
+      <li key={'window_type'}>window_type: {JSON.stringify(tree['window_type'])}</li>
+      
+      <li key={'nodes'}>{'nodes: '}
+        {tree.nodes.length
+          ? tree.nodes.map((node: I3Node) =>
+            <Tree key={node.id} tree={node} />
+          )
+          : 'none'
+        }
+      </li>
+
+      <li key={'floating_nodes'}>floating_nodes: {JSON.stringify(tree['floating_nodes'])}</li>
+      <li key={'focus'}>focus: {JSON.stringify(tree['focus'])}</li>
+      <li key={'fullscreen_mode'}>fullscreen_mode: {JSON.stringify(tree['fullscreen_mode'])}</li>
+      <li key={'sticky'}>sticky: {JSON.stringify(tree['sticky'])}</li>
+      <li key={'floating'}>floating: {JSON.stringify(tree['floating'])}</li>
+      <li key={'swallows'}>swallows: {JSON.stringify(tree['swallows'])}</li>
+  </ul>
+  )
 }
 
 export default App;
